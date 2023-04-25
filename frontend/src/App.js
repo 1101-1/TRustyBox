3fc23415b46266f3c1294d83343bd602f80397ce
kir <k.melkozerov@gmail.com>
2023-04-25 08:15:52 +0500

    feat: Add simple frontend with ReactJS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import UploadForm from './FileUploadForm.js';
 function App() {
   return (
     <div className="App">
-      <h1>My App</h1>
+      <h1>TRustyBox</h1>
       <UploadForm />
     </div>
   );
--- frontend/src/FileUploadForm.js
+++ frontend/src/FileUploadForm.js
@@ -20,7 +20,7 @@ function UploadForm() {
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        setResponseData(data); // Set the response data in state
+        setResponseData(data);
       })
       .catch(error => {
         console.error(error);
@@ -28,33 +28,36 @@ function UploadForm() {
   }
 
   return (
-    <div>
-      <h1>File Upload Form</h1>
-      <form action="/" method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
-        <input type="file" name="file" onChange={handleFileChange} />
-        <input type="submit" value="Upload" />
+    <div className="container">
+      <h1 className="my-4">File Upload Form</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="file" className="form-label">Choose a file:</label>
+          <input type="file" className="form-control" id="file" name="file" onChange={handleFileChange} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="encryption" className="form-label">Encryption:</label>
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="encryption" id="none" value="" checked={!encryptionType} onChange={() => setEncryptionType("")} />
+            <label className="form-check-label" htmlFor="none">
+              None
+            </label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="radio" name="encryption" id="aes" value="aes" checked={encryptionType === "aes"} onChange={() => setEncryptionType("aes")} />
+            <label className="form-check-label" htmlFor="aes">
+              AES
+            </label>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Upload</button>
       </form>
-      <div>
-        <input
-          type="radio"
-          id="aes"
-          name="encryption"
-          value="aes"
-          checked={encryptionType === "aes"}
-          onChange={() => setEncryptionType("aes")}
-        />
-        <label htmlFor="aes">AES</label>
-        <input
-          type="radio"
-          id="none"
-          name="encryption"
-          value=""
-          checked={!encryptionType}
-          onChange={() => setEncryptionType("")}
-        />
-        <label htmlFor="none">None</label>
-      </div>
-      {responseData && <pre>{JSON.stringify(responseData, null, 2)}</pre>} {/* Render the response data */}
+      {responseData && (
+        <div className="mt-4">
+          <h2 className="my-4">Response Data:</h2>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
