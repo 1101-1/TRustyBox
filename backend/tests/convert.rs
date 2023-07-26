007e0bfe76089f89157f24cd2680b6d710bb2a92
Kirill <k.melkozerov@gmail.com>
2023-07-26 15:47:09 +0400

    feat add prod frontend build

--- backend/tests/convert.rs
+++ backend/tests/convert.rs
@@ -1,13 +1,13 @@
 #[tokio::test]
-    async fn convert_correct() {
-        use trustybox_tests::crypt::aes_key::set_aes_key;
-        use trustybox_tests::crypt::base64_convert::convert_aes_to_base64;
-        use trustybox_tests::crypt::base64_convert::convert_base64_to_aes;
+async fn convert_correct() {
+    use trustybox_tests::crypt::aes_key::set_aes_key;
+    use trustybox_tests::crypt::base64_convert::convert_aes_to_base64;
+    use trustybox_tests::crypt::base64_convert::convert_base64_to_aes;
 
-        let key = set_aes_key().await;
-        let base_str = convert_aes_to_base64(key).await;
+    let key = set_aes_key().await;
+    let base_str = convert_aes_to_base64(key).await;
 
-        assert_eq!(convert_base64_to_aes(base_str.clone()).await.unwrap(), key);
+    assert_eq!(convert_base64_to_aes(base_str.clone()).await.unwrap(), key);
 
-        assert_eq!(convert_aes_to_base64(key.clone()).await, base_str);
-    }
+    assert_eq!(convert_aes_to_base64(key.clone()).await, base_str);
+}
